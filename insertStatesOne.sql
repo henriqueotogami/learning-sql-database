36a1ade53dbcca048748c13319851cd5dc4b6dea
HenriqueMAP <henrique.map@outlook.com>
2022-08-10 23:20:39 -0300

    Lecture 290 - Inserting the table of States of Brazil

---
+++ insertStatesOne.sql
@@ -0,0 +1,34 @@
+insert into states(nome, sigla, regiao, populacao)
+values ('Acre', 'AC', 'Norte', 0.89);
+
+INSERT INTO states
+    (nome, sigla, regiao, populacao)
+VALUES
+    ('Alagoas'            , 'AL', 'Nordeste'    , 03.35),
+    ('Amapá'              , 'AP', 'Norte'       , 00.86),
+    ('Amazonas'           , 'AM', 'Norte'       , 04.20),
+    ('Bahia'              , 'BA', 'Nordeste'    , 14.93),
+    ('Ceará'              , 'CE', 'Nordeste'    , 09.18),
+    ('Distrito Federal'   , 'DF', 'Centro-Oeste', 03.05),
+    ('Espírito Santo'     , 'ES', 'Sudeste'     , 04.06),
+    ('Goiás'              , 'GO', 'Centro-Oeste', 07.11),
+    ('Maranhão'           , 'MA', 'Nordeste'    , 07.11),
+    ('Mato Grosso'        , 'MT', 'Centro-Oeste', 03.52),
+    ('Mato Grosso do Sul' , 'MS', 'Centro-Oeste', 02.80),
+    ('Minas Gerais'       , 'MG', 'Sudeste'     , 21.29),
+    ('Paraná'             , 'PR', 'Sul'         , 11.51),
+    ('Paraíba'            , 'PB', 'Nordeste'    , 04.03),
+    ('Pará'               , 'PA', 'Norte'       , 08.69),
+    ('Pernambuco'         , 'PE', 'Nordeste'    , 09.61),
+    ('Piauí'              , 'PI', 'Nordeste'    , 04.03),
+    ('Rio Grande do Norte', 'RN', 'Nordeste'    , 03.53),
+    ('Rio Grande do Sul'  , 'RS', 'Sul'         , 11.42),
+    ('Rio de Janeiro'     , 'RJ', 'Sudeste'     , 17.06),
+    ('Rondônia'           , 'RO', 'Norte'       , 01.79),
+    ('Roraima'            , 'RR', 'Norte'       , 0.63),
+    ('Santa Catarina'     , 'SC', 'Sul'         , 07.25),
+    ('Sergipe'            , 'SE', 'Nordeste'    , 02.31),
+    ('São Paulo'          , 'SP', 'Sudeste'     , 46.28),
+    ('Tocantins'          , 'TO', 'Norte'       , 01.59);
+
+    
