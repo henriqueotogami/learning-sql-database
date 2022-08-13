90741ab66c5e61f49352b1a77a0b431c81c2c14f
HenriqueMAP <henrique.map@outlook.com>
2022-08-13 15:21:46 -0300

    Lecture 294 - Excluding the invalid states of Brazil from the table.

---
+++ src/delete/deleteStates.sql
@@ -0,0 +1,13 @@
+-- Command + Shift + E
+DELETE FROM states 
+WHERE states_acronym = 'MN'
+
+-- Command + Shift + E
+SELECT * FROM states
+
+-- Command + Shift + E
+DELETE FROM states
+WHERE id >= 1000
+
+-- Command + Shift + E
+SELECT * FROM states
