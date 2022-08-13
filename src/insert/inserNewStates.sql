7eb09a4417031e36e618ec6db8a62679fe934371
HenriqueMAP <henrique.map@outlook.com>
2022-08-13 14:51:59 -0300

    Lecture 293 - Defining ID of new states of Brazil in the table.

---
+++ src/insert/inserNewStates.sql
@@ -0,0 +1,13 @@
+-- Command + Shift + E
+INSERT INTO states (id, states_name, states_acronym, states_region, states_population)
+VALUES (1000, 'Novo', 'NV', 'Sul', 2.54)
+
+-- Command + Shift + E
+SELECT * FROM states
+
+-- Command + Shift + E
+INSERT INTO states (states_name, states_acronym, states_region, states_population)
+VALUES ('Mais Novo', 'MN', 'Norte', 2.51)
+
+-- Command + Shift + E
+SELECT * FROM states
