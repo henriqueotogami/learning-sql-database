c729c828751be68b6b2b1fdefae913e1be4c3f76
HenriqueMAP <henrique.map@outlook.com>
2022-08-17 19:36:57 -0300

    Lecture - 301 - adding new mayors of Brazilian cities.

---
+++ src/insert/insertNewMayors.sql
@@ -0,0 +1,19 @@
+-- Command + Shift + E
+SELECT * FROM brazilian_cities;
+
+-- Command + Shift + E
+INSERT INTO mayors
+    (mayor_name, city_id)
+VALUES
+    ('Rodrigo Neves', 2),
+    ('Raquel Lyra', 3),
+    ('Zenaldo Coutinho', null);
+
+-- Command + Shift + E
+SELECT * FROM mayors;
+
+-- Command + Shift + E
+INSERT INTO mayors
+    (mayor_name, city_id)
+VALUES
+    ('Rafael Greca', null);
